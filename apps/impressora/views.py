8d994f1211065c8878b66bb99826b7e7c1fa28a7
Mariana Hammel <anleaes@gmail.com>
2025-06-06 21:46:24 -0300

    views.py

--- apps/impressora/views.py
+++ apps/impressora/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-#git add .
-#git commit -m "Enviando info"
-#git push origin main
-# Create your views here.
 
 from .models import Impressora
 from rest_framework import viewsets
