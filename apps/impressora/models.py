95939f41e53061d09aa600c67b8a350f1c136a66
Mariana Hammel <anleaes@gmail.com>
2025-06-06 21:37:16 -0300

    Criei a classe impressora

--- apps/impressora/models.py
+++ apps/impressora/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from tipo.models import Category
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    marca = models.CharField('Marca', max_length=50)
+    precoDiario = models.DecimalField('Preço Diário', max_digits=10, decimal_places=2) 
+    status = models.CharField('Status', max_length=20, choices=[('disponivel', 'Disponível'), ('indisponivel', 'Indisponível')], default='disponivel')
+    class Meta:
+        verbose_name = 'Impressora'
+        verbose_name_plural = 'Impressoras'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
+#Products - Impressora
+#class Impressora {
+   # - nome: String
+   # - marca: String
+   # - precoDiario: double
+   # - status: String
+   # --
+   # + toString(): String
+#}
