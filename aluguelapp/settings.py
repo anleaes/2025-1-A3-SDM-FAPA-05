a714b31e68fc7bc6473775b41cd0136663227073
Mariana Hammel <anleaes@gmail.com>
2025-06-06 21:33:15 -0300

    'products.apps.ProductsConfig', add ao installed apps

--- aluguelapp/settings.py
+++ aluguelapp/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'tipo.apps.TipoConfig',
+    'impressora.apps.ImpressoraConfig',
+
 
 ]
 
--- apps/impressora/views.py
+++ apps/impressora/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+#git add .
+#git commit -m "Enviando info"
+#git push origin main
 # Create your views here.
