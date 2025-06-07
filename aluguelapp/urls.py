cc6a2f84464f9db1a33794635f53b284ae8d0f2c
Mariana Hammel <anleaes@gmail.com>
2025-06-06 21:48:15 -0300

    add o urls.py na raíz(urls.py)

--- aluguelapp/urls.py
+++ aluguelapp/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tipos/', include('tipos.urls', namespace='Tipos')),
+    path('produtos/', include('products.urls', namespace='products')),
+
 ]
 #a
