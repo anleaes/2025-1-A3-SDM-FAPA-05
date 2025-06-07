5d20e42d6384963d9670738ddba6ee3405a8173d
Mariana Hammel <anleaes@gmail.com>
2025-06-06 21:45:05 -0300

    Ajuste: serializers to serializer

--- apps/impressora/views.py
+++ apps/impressora/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 #git commit -m "Enviando info"
 #git push origin main
 # Create your views here.
+
+from .models import Impressora
+from rest_framework import viewsets
+from .serializer import ImpressoraSerializer
+
+# Após o comentario "# Create your views here." e crie as views "Product".
+
+class ImpressoraViewSet(viewsets.ModelViewSet):
+    queryset = Impressora.objects.all()
+    serializer_class = ImpressoraSerializer  
