20dc822c90d7df381aa65e8dcb9e176896cd0ae5
Mariana Hammel <anleaes@gmail.com>
2025-06-06 21:09:20 -0300

    criacao da classe tipo

--- apps/tipo/models.py
+++ apps/tipo/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Tipo(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descricao', max_length=100)
+    velocidade = models.IntegerField('Velocidade')
+    resolucao = models.CharField('Resolucao', max_length=20, blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
+    
+    #class Tipo {
+   # - nome: String
+   # - descricao: String
+   # - velocidade: int
+   # - resolucao: String
+   # --
+   # + toString(): String
