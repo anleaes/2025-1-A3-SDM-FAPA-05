532f361bb7d8c6ac1bbf5740143e9fe9a7b3477b
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-17 14:36:46 -0300

    ajustei o erro: 'clientes.apps.ProductsConfig', para 'clientes.apps.ClientesConfig',

--- aluguelapp/settings.py
+++ aluguelapp/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'tipos.apps.TiposConfig',
-    'clientes.apps.ProductsConfig',
+    'clientes.apps.ClientesConfig',
 
 
 ]
