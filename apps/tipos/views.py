2ae6275e74acb0fd68548dc182fe6d3ae81ce480
Mariana Hammel <hammel2006@gmail.com>
2025-06-19 11:33:46 -0300

    views.py em apps/tipos/views.py

--- apps/tipos/views.py
+++ apps/tipos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Tipo
+from rest_framework import viewsets
+from .serializer import TipoSerializer
+
+# Após o comentario "# Create your views here." e crie as views "Category".
+
+class TipoViewSet(viewsets.ModelViewSet):
+    queryset = Tipo.objects.all()
+    serializer_class = TipoSerializer 
