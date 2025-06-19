933bfe493fdd6d8758bf77ffb79570ac1165e6ea
Mariana Hammel <hammel2006@gmail.com>
2025-06-19 10:55:45 -0300

    alteração no settings.py. Eu tirei o comentário da secret key

--- aluguel_impressora/settings.py
+++ aluguel_impressora/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-6ip@crt#=)8pl)vl_qnuk$9bkk9-&+4=@!v5dqn6!%=%^i%yq6'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
