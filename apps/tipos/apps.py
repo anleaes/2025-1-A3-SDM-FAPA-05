6e6aa36634d329ee3d0e0686b46b36634ce08795
Mariana Hammel <hammel2006@gmail.com>
2025-06-19 10:09:09 -0300

    Apaguei tudo... Pois nada estava funcionando. :D (no github)

--- apps/tipos/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class TiposConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "tipos"
-
-verbose_name = 'Tipos'
--- apps/tipos/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import Tipos
-from rest_framework import serializers
-
-class TiposSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tipo
-        fields = '__all__'
