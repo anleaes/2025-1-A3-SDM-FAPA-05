f6921ee2c46831026010f1e42c41261147f9ee5c
Mariana Hammel <hammel2006@gmail.com>
2025-06-19 11:26:18 -0300

    "serializer.py" dentro do app "tipo"

---
+++ apps/tipos/serealizer.py
@@ -0,0 +1,9 @@
+
+from .models import Tipo
+from rest_framework import serializers
+
+class TipoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tipo
+        fields = '__all__'
+        
