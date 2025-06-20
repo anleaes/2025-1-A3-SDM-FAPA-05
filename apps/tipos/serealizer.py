95bbea2b4796c71dcba0687d2eb113902a82cbd2
Mariana Hammel <hammel2006@gmail.com>
2025-06-20 00:40:37 -0300

    apaguei tudo, de novo...

--- apps/tipos/serealizer.py
+++
@@ -1,9 +0,0 @@
-
-from .models import Tipo
-from rest_framework import serializers
-
-class TipoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tipo
-        fields = '__all__'
-        
