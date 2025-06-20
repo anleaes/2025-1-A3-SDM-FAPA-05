b585552d77a3ff8d00112337a7f24c6246a832c5
Mariana Hammel <hammel2006@gmail.com>
2025-06-20 01:13:00 -0300

    criação ".env" na raíz do projeto

--- aluguel_app/settings.py
+++ aluguel_app/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-6w4$a@h7jn8pe73l%o#*(25wrlnspc0wy)i9v=*$b(43c5u(#('
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
