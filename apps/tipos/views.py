919c7fb4f53cd420a76cbadb96858a47fcf92fda
Mariana Hammel <hammel2006@gmail.com>
2025-06-21 19:15:18 -0300

    criei a view impressora

--- apps/tipos/views.py
+++ apps/tipos/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-
+from .models import Impressora
+from rest_framework import viewsets
+from .serializer import ImpressoraSerializer
 # Create your views here.
 
-
-from .models import Tipo
-from rest_framework import viewsets
-from .serializer import TipoSerializer
-
-# Após o comentario "# Create your views here." e crie as views "Category".
-
-class TipoViewSet(viewsets.ModelViewSet):
-    queryset = Tipo.objects.all()
-    serializer_class = TipoSerializer  
+class ImpressoraViewSet(viewsets.ModelViewSet):
+    queryset = Impressora.objects.all()
+    serializer_class = ImpressoraSerializer  
+    
