16cacae14344dba471c622eb9a3387313b030b6f
Mariana Hammel <hammel2006@gmail.com>
2025-06-21 19:05:09 -0300

    add Impressora model

--- apps/tipos/models.py
+++ apps/tipos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from tipos.models import Tipo  
 
 # Create your models here.
+
+class Impressora(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    marca = models.CharField('Marca', max_length=50)
+    preco_diario = models.DecimalField('Preço Diário', max_digits=8, decimal_places=2)
+    status = models.CharField('Status', max_length=20)
+    modelo = models.CharField('Modelo', max_length=50)
+
+    class Meta:
+        verbose_name = 'Impressora'
+        verbose_name_plural = 'Impressoras'
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.name} - {self.marca} ({self.modelo})'
