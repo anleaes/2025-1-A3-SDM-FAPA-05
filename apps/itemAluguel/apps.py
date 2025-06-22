9d0473631560a6ab3b7850c6c10418f474061a75
Mariana Hammel <hammel2006@gmail.com>
2025-06-22 15:43:56 -0300

    add verbose name: "verbose_name = 'Item Aluguel'

--- apps/itemAluguel/apps.py
+++ apps/itemAluguel/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class ItemaluguelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'itemAluguel'
+    verbose_name = 'Item Aluguel'
+
