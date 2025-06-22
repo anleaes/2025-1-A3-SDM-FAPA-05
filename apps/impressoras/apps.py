c0ac59cd44fb8fc718ef2fda73d115848c9d94e3
Mariana Hammel <hammel2006@gmail.com>
2025-06-22 13:59:21 -0300

    add verbose_name no ImpressorasConfig em apps.py

--- apps/impressoras/apps.py
+++ apps/impressoras/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ImpressorasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'impressoras'
+    verbose_name = 'Impressoras'
