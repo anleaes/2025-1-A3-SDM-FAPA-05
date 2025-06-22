dc3070a659ce9b06b64be8071ce4ff04bce7262d
Mariana Hammel <hammel2006@gmail.com>
2025-06-22 15:52:16 -0300

    criação do models em apps/itemaluguel/models.py

--- apps/itemAluguel/models.py
+++ apps/itemAluguel/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Itemaluguel(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    descricao = models.TextField('Descricao', max_length=100)
+    quantidadedias = models.TextField('Quantidade de Dias', max_length=100)
+    preco = models.TextField('Preço Total', max_length=100)
+    obs = models.TextField('Observacoes', max_length=100)
+
+    class Meta:
+        verbose_name = 'Item so Aluguel'
+        verbose_name_plural = 'Itens do Aluguel'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
