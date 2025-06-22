7bcb70058221f9991cf2c9abf5ec61aa17b3a7aa
Mariana Hammel <hammel2006@gmail.com>
2025-06-22 13:33:52 -0300

    add Tipo model and serializer for tipos app

--- apps/tipos/models.py
+++ apps/tipos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Tipo(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descricao', max_length=100)
+    velocidade = models.CharField('Velocidade', max_length=50)
+    resolucao = models.TextField('Resolucao', max_length=100)
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
