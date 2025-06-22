79e17e8362b5e0a5c109f5ef17ded4496cd9fc5d
Mariana Hammel <hammel2006@gmail.com>
2025-06-22 20:13:26 -0300

    add caminho

--- aluguel_app/urls.py
+++ aluguel_app/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('impressoras/', include('impressoras.urls', namespace='impressoras')),
+    path('clientes/', include('clientes.urls', namespace='clientes')),
 
 ]
