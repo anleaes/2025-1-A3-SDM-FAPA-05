aeee77419991ae6731fb6f4715c3c0fa67714224
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-23 22:07:23 -0300

    adicinei o caminho de impressoras

--- appaluguel/urls.py
+++ appaluguel/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('categorias/', include('categorias.urls', namespace='categorias')),    
+    path('impressoras/', include('impressoras.urls', namespace='impressoras')),
 
 ]
