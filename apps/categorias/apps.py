678fb7dfb28439e9d68a0d65364a5c8066498f0d
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-23 21:07:59 -0300

    corrigi o nome da classe de configuração do app 'categorias' e ajustei o nome do app para 'categorias'

--- apps/categorias/apps.py
+++ apps/categorias/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
-class CategoriesConfig(AppConfig):
+class CategoraesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "categories"
+    name = "categorias"
+    verbose_name = 'Categorias'
+
