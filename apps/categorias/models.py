d06e774a06b471d3820ef24ead6250f71d2bdf99
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-23 21:56:02 -0300

    renomeei o modelo Category para Categoria e atualizei os campos relacionados no app impressoras

--- apps/categorias/models.py
+++ apps/categorias/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
+class Categoria(models.Model):
     name = models.CharField('Nome', max_length=50)
     description = models.TextField('Descricao', max_length=100)
     speed = models.CharField('Velocidade', max_length=30)
