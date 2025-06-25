99ced7585749be2261e666643a2844a065e36ff0
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-24 23:09:24 -0300

    verbose name em pedido item

--- apps/pedidoitem/apps.py
+++ apps/pedidoitem/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PedidoitemConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pedidoitem"
+    verbose_name = 'Pedido Item'
