676a1d48ddfc4664d222990d6835ef4859cea8bc
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-25 18:43:32 -0300

    recomeçando o projeto do zero.

--- apps/clientepromocao/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClientepromocaoConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "clientepromocao"
-    verbose_name = 'Cliente Promoção'
--- apps/pedidoitem/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PedidoitemConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "pedidoitem"
-    verbose_name = 'Pedido Item'
