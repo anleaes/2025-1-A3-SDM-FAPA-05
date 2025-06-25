6d6ffd71cf550182b425a2d871f6518b4ba2c1fb
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-24 23:18:05 -0300

    models impressora

--- apps/impressoras/models.py
+++ apps/impressoras/models.py
@@ -1,34 +1,16 @@
-from django.db import models
-from categories.models import Categoria
-
-# Create your models here.
-
-from django.db import models
-
-class Categoria(models.Model): 
-    nome = models.CharField(max_length=100)
-    descricao = models.TextField()
-
-    def __str__(self):
-        return self.nome
+from categorias.models import Categoria
 
 class Impressora(models.Model):
-    marca = models.CharField('Marca', max_length=50)
-    modelo = models.CharField('Modelo', max_length=50)
-    preco = models.DecimalField('Preço por dia', max_digits=10, decimal_places=2)
-    
-    STATUS_CHOICES = [
-        ('em_uso', 'Em uso'),
-        ('disponivel', 'Disponível'),
-    ]
-    status = models.CharField('Status', max_length=20, choices=STATUS_CHOICES, default='disponivel')
-
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name='Categoria')
+    name = models.CharField('Nome', max_length=50)
+    marca = models.TextField('Marca', max_length=50)
+    date_fabrication = models.DateField('Data Fabricacao', auto_now=False, auto_now_add=False) 
+    disponivel = models.BooleanField('Disponivel', default=False)
+    categoria = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Impressora'
         verbose_name_plural = 'Impressoras'
-        ordering = ['id']
+        ordering =['id']
 
     def __str__(self):
-        return f"{self.marca} {self.modelo}"
+        return self.name
