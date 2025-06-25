3a3e42671c90f025c91fd14302b718423beca24f
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-25 19:30:12 -0300

    feat: implementa modelos iniciais

--- apps/categorias/models.py
+++ apps/categorias/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    descricao = models.TextField('Descrição', max_length=200)
+    codigo = models.CharField('Código', max_length=20, unique=True)
+    ativa = models.BooleanField('Ativa', default=True)
+    
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
+
