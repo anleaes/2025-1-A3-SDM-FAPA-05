5359129b0225177cdb62c68e03f2503890b594de
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-24 23:07:32 -0300

    verbose name em cliente/apps.py

--- apps/clientepromocao/apps.py
+++ apps/clientepromocao/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ClientepromocaoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "clientepromocao"
+    verbose_name = 'Cliente Promoção'
