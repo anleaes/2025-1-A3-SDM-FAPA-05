e6c3a8b9d81ebdec5d549e91e82164074cd30382
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-26 17:48:29 -0300

    alterações na branch views

--- apps/printers/views.py
+++ apps/printers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Printer
+from rest_framework import viewsets
+from .serializer import PrinterSerializer
 # Create your views here.
+
+class PrinterViewSet(viewsets.ModelViewSet):
+    queryset = Printer.objects.all()
+    serializer_class = PrinterSerializer  
