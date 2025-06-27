88debb97f6551c0623f10cd97ed43fbe07f47a83
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-27 16:31:06 -0300

    página home p/ facilitar o acesso

--- aluguelapp/urls.py
+++ aluguelapp/urls.py
@@ -1,31 +1,56 @@
-"""
-URL configuration for aluguelapp project.
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf.urls.static import static
+# from django.conf import settings
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 
+
+# urlpatterns = [
+#    # path("", home, name="home"), 
+#     path("admin/", admin.site.urls),
+#     path('categorias/', include('categories.urls', namespace='categories')),    
+#     path('impressoras/', include('printers.urls', namespace='printers') ),
+#     path('documentos/', include('documents.urls', namespace='documents')), 
+#     path('clientes/', include('clients.urls', namespace='clients')),
+#     path('fornecedores/', include('fornecedores.urls', namespace='fornecedores')),
+#     path('pedidos/', include('orders.urls', namespace='orders')),
+#     path('itens_pedido/', include('orderitems.urls', namespace='orderitems')),
+
+# ]
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.http import HttpResponse
+
+def home(request):
+    html = """
+    <h1>Bem-vindo ao Sistema!</h1>
+    <p>Escolha uma das opções abaixo:</p>
+    <ul>
+        <li><a href="/admin/">Admin</a></li>
+        <li><a href="/categorias/">Categorias</a></li>
+        <li><a href="/impressoras/">Impressoras</a></li>
+        <li><a href="/documentos/">Documentos</a></li>
+        <li><a href="/clientes/">Clientes</a></li>
+        <li><a href="/fornecedores/">Fornecedores</a></li>
+        <li><a href="/pedidos/">Pedidos</a></li>
+        <li><a href="/itens_pedido/">Itens do Pedido</a></li>
+    </ul>
+    """
+    return HttpResponse(html)
 
 urlpatterns = [
+    path("", home, name="home"), 
     path("admin/", admin.site.urls),
     path('categorias/', include('categories.urls', namespace='categories')),    
-    path('impressoras/', include('printers.urls', namespace='printers') ),
+    path('impressoras/', include('printers.urls', namespace='printers')),
     path('documentos/', include('documents.urls', namespace='documents')), 
     path('clientes/', include('clients.urls', namespace='clients')),
     path('fornecedores/', include('fornecedores.urls', namespace='fornecedores')),
     path('pedidos/', include('orders.urls', namespace='orders')),
     path('itens_pedido/', include('orderitems.urls', namespace='orderitems')),
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
