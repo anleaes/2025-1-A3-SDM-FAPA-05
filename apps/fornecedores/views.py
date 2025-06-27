b19684a86c5284e12a5818c03faaab8d8fcd97dc
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-27 14:23:01 -0300

    import FornecedorSerializer na views.py

--- apps/fornecedores/views.py
+++ apps/fornecedores/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Fornecedor
-from .serializers import FornecedorSerializer
+from .serializer import FornecedorSerializer
 # Create your views here.
 
 class FornecedorViewSet(viewsets.ModelViewSet):
