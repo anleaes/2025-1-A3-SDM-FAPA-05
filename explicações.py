8712163be78fe641253e272f7356e1488ac39cce
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-27 16:30:30 -0300

    Criei essa pasta para caso eu queira mudar a forma da filtragem

---
+++ explicações.py
@@ -0,0 +1,10 @@
+# Exemplos de uso de QuerySet
+
+# Carregando todos os produtos
+#queryset = Product.objects.all()
+
+# Filtrando produtos
+#queryset = Product.objects.filter()
+
+# Filtrando e ordenando por categoria decrescente
+#queryset = Product.objects.filter().order_by('-category')
