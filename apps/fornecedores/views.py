39710d353affba72cf3d2fbac5cec5f4549e6eb6
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-27 15:19:02 -0300

    removi imports q estavam duplicados

--- apps/fornecedores/views.py
+++ apps/fornecedores/views.py
@@ -3,17 +3,6 @@ from rest_framework import viewsets
 from .models import Fornecedor
 from .serializer import FornecedorSerializer
 # Create your views here.
-
-<<<<<<< HEAD
-from .models import Fornecedor
-from rest_framework import viewsets
-from .serializer import FornecedorSerializer
-
 class FornecedorViewSet(viewsets.ModelViewSet):
     queryset = Fornecedor.objects.all()
     serializer_class = FornecedorSerializer
-=======
-class FornecedorViewSet(viewsets.ModelViewSet):
-    queryset = Fornecedor.objects.all()
-    serializer_class = FornecedorSerializer
->>>>>>> main
