f776115a77d17185f89ff62e1afaa934332c9eef
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-27 13:11:39 -0300

    feat(views): adiciona viewset Fornecedor

--- apps/fornecedores/views.py
+++ apps/fornecedores/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Fornecedor
+from rest_framework import viewsets
+from .serializer import FornecedorSerializer
+
+class FornecedorViewSet(viewsets.ModelViewSet):
+    queryset = Fornecedor.objects.all()
+    serializer_class = FornecedorSerializer
