b060d30b52f3d7e456716cc2e4a9046d88f11dd6
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-27 13:10:19 -0300

    feat(serializer): adiciona serializer Fornecedor

---
+++ apps/fornecedores/serializer.py
@@ -0,0 +1,8 @@
+
+from .models import Fornecedor
+from rest_framework import serializers
+
+class FornecedorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fornecedor
+        fields = '__all__'
