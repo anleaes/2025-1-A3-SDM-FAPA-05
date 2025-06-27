b035a3550340084ff95568e58ff3e83f298b43e5
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-27 15:18:43 -0300

    corrigindo nome

--- apps/orders/serealizer.py
+++
@@ -1,8 +0,0 @@
-
-from .models import Order
-from rest_framework import serializers
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = '__all__'
