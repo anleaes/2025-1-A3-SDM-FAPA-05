b355df9c878755386c1b902a0b5bef7c22db308e
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-27 16:42:19 -0300

    Implementação de Search

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from .models import Category
 from rest_framework import viewsets
 from .serializer import CategorySerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer  
-    
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
