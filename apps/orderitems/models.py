9e60f4562b055a51b229481adc1f22cf0cd85671
Mariana Hammel <165729032+marihammel@users.noreply.github.com>
2025-06-27 15:29:32 -0300

    Implement RentalItem model

--- apps/orderitems/models.py
+++ apps/orderitems/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from products.models import Product
+from orders.models import Order
 # Create your models here.
+
+class RentalItem(models.Model):
+    quantity = models.PositiveIntegerField('Quantidade', null=True, blank=True, default=1)
+    unitary_price = models.DecimalField('Preço Unitário', max_digits=10, decimal_places=2)
+    printer = models.ForeignKey(Printer, on_delete=models.CASCADE, verbose_name='Impressora')
+    rental_order = models.ForeignKey(RentalOrder, on_delete=models.CASCADE, related_name='itens', verbose_name='Pedido de Aluguel')
+
+    class Meta:
+        unique_together = ('rental_order', 'printer')
+        verbose_name = 'Item de Aluguel'
+        verbose_name_plural = 'Itens de Aluguel'
+        ordering = ['id']
+
+    def __str__(self):
+        return f"{self.quantity}x {self.printer.name} no pedido #{self.rental_order.id}"
